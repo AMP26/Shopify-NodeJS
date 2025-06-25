2851a5a223eff979f97f2b1c3d8a5dbc210a3a98
AMP26 <amit.patnaik0126@gmail.com>
2025-06-25 16:43:08 +0530

    .env and dbconfig files edited

--- shopify-inventory-and-product-management/config/dbconfig.js
+++ shopify-inventory-and-product-management/config/dbconfig.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize("publicshopifyapp", "root", "Amitpats#11", {
+const sequelize = new Sequelize("publicshopifyapp", "root", "<password>", {
   host: "localhost",
   dialect: "mysql",
   logging: false
